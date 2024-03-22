b6a6a0824063cbd85bafb1c1f481bda9940bfbc5
John Heneghan <jwheneghan@hotmail.com>
2024-03-22 14:43:49 -0230

    Added Carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
+import UncontrolledExample from "./Carousel";
 
 function App() {
   return (
     <>
       <Navbar />
+      <UncontrolledExample />
     </>
   );
 }
---
+++ src/Carousel.tsx
@@ -0,0 +1,35 @@
+import Carousel from "react-bootstrap/Carousel";
+import One from "./assets/One.jpg";
+import Two from "./assets/Two.jpg";
+import Three from "./assets/Three.jpg";
+function UncontrolledExample() {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img src={One} />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={Two} />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={Three} />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default UncontrolledExample;
