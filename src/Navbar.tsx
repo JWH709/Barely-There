ae501ed5d0754c5e4574e6f7636e71f6002a68ea
JWH709 <jwheneghan@hotmail.com>
2024-03-25 16:16:52 -0230

    Navbar broken, needs fix

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,14 +3,18 @@ import React, { useState } from "react";
 const Navbar = () => {
   const links = [
     [
-      {
-        title: "Swimming",
-        url: "https://swimming709.bandcamp.com/album/demo-2019",
-      },
-      {
-        title: "Wiremouth",
-        url: "https://wiremouth709.bandcamp.com/",
-      },
+      { title: "Birch Beer", url: "" },
+      { title: "Already Dead", url: "" },
+      { title: "El Toro", url: "" },
+      { title: "Kerosene", url: "" },
+      { title: "Gallery", url: "" },
+      { title: "Monroe", url: "" },
+      { title: "Intern", url: "" },
+      { title: "WIREMOUTH", url: "" },
+      { title: "Hollow Grave", url: "" },
+      { title: "Swimming", url: "" },
+      { title: "CaT CaFé", url: "" },
+      { title: "Bugfight", url: "" },
     ],
     [
       {
@@ -89,6 +93,7 @@ interface DropDownProps {
 }
 
 const Dropdown = (props: DropDownProps) => {
+  //Dropdown functionality. Appears on hover.
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseOver = () => {
@@ -98,46 +103,119 @@ const Dropdown = (props: DropDownProps) => {
   const handleMouseOut = () => {
     setIsHovered(false);
   };
+  //functions for grid:
 
-  return (
-    <>
-      <li
-        className="nav-item dropdown"
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
-      >
-        <a
-          className="nav-link dropdown-toggle"
-          href="#"
-          role="button"
-          data-bs-toggle="dropdown"
-          aria-expanded={isHovered}
-        >
-          {props.title}
-        </a>
-        <ul
-          className={`dropdown-menu ${isHovered ? "show" : ""}`}
+  const testStorage: { title: string; url: string }[][] = [];
+
+  function getRows(items): void {
+    const numberOfRows: number = Math.round(items.length / 3);
+
+    for (let i = 0; i < numberOfRows; i++) {
+      const isFirstRow: boolean = i === 0;
+      const bandBenchmark: number = testStorage[0]?.length * i || 0;
+
+      testStorage.push(
+        isFirstRow
+          ? [items[i], items[i + 1], items[i + 2]].filter(
+              (item) => item !== undefined
+            )
+          : [
+              items[bandBenchmark],
+              items[bandBenchmark + 1],
+              items[bandBenchmark + 2],
+            ].filter((item) => item !== undefined)
+      );
+    }
+
+    let itemTotal: number = 0;
+    for (let i = 0; i < testStorage.length; i++) {
+      itemTotal += testStorage[i].length;
+    }
+
+    if (itemTotal < items.length) {
+      testStorage.push([items[itemTotal]]);
+    }
+  }
+
+  //is list or grid?:
+
+  if (props.items.length < 4)
+    return (
+      <>
+        <li
+          className="nav-item dropdown"
           onMouseOver={handleMouseOver}
           onMouseOut={handleMouseOut}
         >
-          {props.items.map((item, index) => (
-            <React.Fragment key={index}>
-              <li>
-                <a className="dropdown-item" href={item.url} target="_blank">
-                  {item.title}
-                </a>
-              </li>
-              {index < props.items.length - 1 && (
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded={isHovered}
+          >
+            {props.title}
+          </a>
+          <ul
+            className={`dropdown-menu ${isHovered ? "show" : ""}`}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+          >
+            {props.items.map((item, index) => (
+              <React.Fragment key={index}>
                 <li>
-                  <hr className="dropdown-divider" />
+                  <a className="dropdown-item" href={item.url} target="_blank">
+                    {item.title}
+                  </a>
                 </li>
-              )}
-            </React.Fragment>
-          ))}
-        </ul>
-      </li>
-    </>
-  );
+                {index < props.items.length - 1 && (
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                )}
+              </React.Fragment>
+            ))}
+          </ul>
+        </li>
+      </>
+    );
+  else
+    return (
+      <>
+        <li
+          className="nav-item dropdown"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          <a
+            className="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded={isHovered}
+          >
+            {props.title}
+          </a>
+          <div
+            className={`dropdown-menu container text-center ${
+              isHovered ? "show" : ""
+            }`}
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+          >
+            <div className="container text-center">
+              {props.items.map((item, index) => (
+                <React.Fragment key={index}>
+                  <div className="row row-cols-3">
+                    <div className="col">{item.title}</div>
+                  </div>
+                </React.Fragment>
+              ))}
+            </div>
+          </div>
+        </li>
+      </>
+    );
 };
 
 export default Navbar;
